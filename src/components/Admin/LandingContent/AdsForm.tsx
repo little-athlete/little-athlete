313bfbfeca400b592df16b7e2bb86bf2c3bfdcee
Fauzan Akmal <fauzanakmal.business@gmail.com>
2025-02-09 09:28:59 +0700

    add ads form

--- src/components/Admin/LandingContent/AdsForm.tsx
+++ src/components/Admin/LandingContent/AdsForm.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { LandingContentProps } from '@/components/Admin/LandingContent/LandingContentProps'
+import Input from '@/components/InputField/InputField'
 
-const AdsForm = ({contentData, setContentData}: LandingContentProps) => {
+const AdsForm = ({ contentData, setContentData }: LandingContentProps) => {
 	return (
-		<div>AdsSection</div>
+		<div className="mt-8 flex flex-col gap-4">
+			<Input
+				label={'Ads Title'}
+				value={contentData?.ads_title}
+				onChange={(e) => setContentData('ads_title', e.target.value)}
+			/>
+			<Input
+				label={'Ads WA Url'}
+				value={contentData?.ads_wa_url}
+				onChange={(e) => setContentData('ads_wa_url', e.target.value)}
+			/>
+		</div>
 	)
 }
 
