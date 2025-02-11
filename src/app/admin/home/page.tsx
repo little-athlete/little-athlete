d8f82b09efe735e0138e1a2e0d22b54026514476
aldam3sena <aldam@karadigital.co>
2025-02-11 23:22:07 +0700

    remove

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -1,34 +1,11 @@
 'use client'
-import Box, { BoxVariant } from '@/components/Box'
-import Card from '@/components/Card/Card'
-import Input, { InputType } from '@/components/InputField/InputField'
-import Typography, { FontWeight } from '@/components/Typography/Typography'
-import React from 'react'
+import Box from '@/components/Box'
+import Typography from '@/components/Typography/Typography'
 
 const AdminHomePage = () => {
 	return (
 		<Box className="mt-6 w-1/2 px-6 xl:px-12">
 			<Typography>Welcome!!</Typography>
-			<Card className="bg-slate-200">
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Typography fontWeight={FontWeight.SemiBold}>asjndjas</Typography>
-				</Box>
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Input type={InputType.Text} label={'Home'} placeholder={''} required />
-				</Box>
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Input type={InputType.Email} label={'jansd'} placeholder={''} />
-				</Box>
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Input type={InputType.Email} label={'jansd'} placeholder={''} />
-				</Box>
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Input type={InputType.Email} label={'jansd'} placeholder={''} />
-				</Box>
-				<Box spaceVertical variant={BoxVariant.Medium}>
-					<Input type={InputType.Email} label={'jansd'} placeholder={''} />
-				</Box>
-			</Card>
 		</Box>
 	)
 }
