b541d8e2307e9acaaa732b363e93026ac8cb18bf
aldam3sena <aldam@karadigital.co>
2025-02-22 08:44:12 +0700

    change our program

--- src/components/landing/OurProgram.tsx
+++ src/components/landing/OurProgram.tsx
@@ -1,37 +1,20 @@
-import { ILandingPage } from '@/db/firestore/interfaces/landing'
-import React, { JSX } from 'react'
-import BaseballIcon from '@/components/Icon/IconBaseball'
-import BasketBallIcon from '@/components/Icon/IconBasketball'
-import FootballIcon from '@/components/Icon/IconFootball'
-import TennisIcon from '@/components/Icon/IconTenis'
 import MaskingImage1Icon from '@/components/Icon/Masking_1'
 import MaskingImage2Icon from '@/components/Icon/Masking_2'
 import MaskingImage3Icon from '@/components/Icon/Masking_3'
 import MaskingImage4Icon from '@/components/Icon/Masking_4'
+import { ILandingPage } from '@/db/firestore/interfaces/landing'
 import Image from 'next/image'
+import React from 'react'
 
 interface ProgramDataType {
 	data: ILandingPage
 }
 
 const OurProgramSection: React.FC<ProgramDataType> = ({ data }) => {
-	const iconMap: Record<string, JSX.Element> = {
-		Basketball: <BasketBallIcon className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]" />,
-		Football: <FootballIcon className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]" />,
-		Baseball: <BaseballIcon className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]" />,
-		Tennis: <TennisIcon className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]" />,
-	}
-
-	const reverse: Record<string, boolean> = {
-		Basketball: false,
-		Football: true,
-		Baseball: false,
-		Tennis: true,
-	}
-
-	const getMaskingComponent = (name: string, imageUrl: string): JSX.Element | null => {
-		const maskingComponents: Record<string, JSX.Element> = {
-			Basketball: (
+	const maskingComponents = [
+		{
+			reverse: false,
+			component: (imageUrl: string) => (
 				<div className="relative">
 					<Image
 						width={474}
@@ -46,7 +29,19 @@ const OurProgramSection: React.FC<ProgramDataType> = ({ data }) => {
 					/>
 				</div>
 			),
-			Football: (
+			icon: (
+				<Image
+					src={'/basketball.png'}
+					alt="program-basketball"
+					className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]"
+					width={32}
+					height={32}
+				/>
+			),
+		},
+		{
+			reverse: true,
+			component: (imageUrl: string) => (
 				<div className="relative">
 					<Image
 						width={515}
@@ -61,7 +56,19 @@ const OurProgramSection: React.FC<ProgramDataType> = ({ data }) => {
 					/>
 				</div>
 			),
-			Baseball: (
+			icon: (
+				<Image
+					src={'/soccer.png'}
+					alt="program-basketball"
+					className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]"
+					width={32}
+					height={32}
+				/>
+			),
+		},
+		{
+			reverse: false,
+			component: (imageUrl: string) => (
 				<div className="relative">
 					<Image
 						width={500}
@@ -76,7 +83,19 @@ const OurProgramSection: React.FC<ProgramDataType> = ({ data }) => {
 					/>
 				</div>
 			),
-			Tennis: (
+			icon: (
+				<Image
+					src={'/baseball-program.png'}
+					alt="program-basketball"
+					className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]"
+					width={32}
+					height={32}
+				/>
+			),
+		},
+		{
+			reverse: true,
+			component: (imageUrl: string) => (
 				<div className="relative">
 					<Image
 						width={490}
@@ -91,82 +110,26 @@ const OurProgramSection: React.FC<ProgramDataType> = ({ data }) => {
 					/>
 				</div>
 			),
-		}
+			icon: (
+				<Image
+					src={'/tennis.png'}
+					alt="program-basketball"
+					className="h-[32px] w-[32px] lg:h-[63px] lg:w-[63px]"
+					width={32}
+					height={32}
+				/>
+			),
+		},
+	]
 
-		return maskingComponents[name] || null
-	}
-
-	// const maskingMap: Record<string, JSX.Element> = {
-	// 	Basketball: (
-	// 		<div className="relative">
-	// 			<Image
-	// 				width={474}
-	// 				height={685}
-	// 				alt="image"
-	// 				src="/Blob 1.png"
-	// 				className="absolute -bottom-1 right-2 h-[90%] w-[90%] lg:bottom-2 lg:right-8 lg:h-[474px] lg:w-[685px]"
-	// 			/>
-	// 			<MaskingImage1Icon
-	// 				src="/program_1.png"
-	// 				className="h-full w-full lg:h-[615px] lg:w-[770px]"
-	// 			/>
-	// 		</div>
-	// 	),
-	// 	Football: (
-	// 		<div className="relative">
-	// 			<Image
-	// 				width={515}
-	// 				height={689}
-	// 				alt="image"
-	// 				src="/Blob 2.png"
-	// 				className="absolute -bottom-1 right-2 h-[90%] w-[90%] rotate-12 lg:bottom-2 lg:right-8 lg:h-[515px] lg:w-[689px]"
-	// 			/>
-	// 			<MaskingImage2Icon
-	// 				src="/program_2.png"
-	// 				className="h-full w-full lg:h-[615px] lg:w-[770px]"
-	// 			/>
-	// 		</div>
-	// 	),
-	// 	Baseball: (
-	// 		<div className="relative">
-	// 			<Image
-	// 				width={500}
-	// 				height={669}
-	// 				alt="blob"
-	// 				src="/Blob 3.png"
-	// 				className="absolute right-2 top-0 h-[90%] w-[90%] lg:right-0 lg:top-10 lg:h-[501px] lg:w-[669px]"
-	// 			/>
-	// 			<MaskingImage3Icon
-	// 				src="/program_3.png"
-	// 				className="h-full w-full lg:h-[615px] lg:w-[770px]"
-	// 			/>
-	// 		</div>
-	// 	),
-	// 	Tennis: (
-	// 		<div className="relative">
-	// 			<Image
-	// 				width={490}
-	// 				height={680}
-	// 				alt="blob"
-	// 				src="/Blob 4.png"
-	// 				className="absolute right-2 top-8 h-[80%] w-[90%] -rotate-3 lg:right-[40px] lg:top-14 lg:h-[490px] lg:w-[680px]"
-	// 			/>
-	// 			<MaskingImage4Icon
-	// 				src="/program_4.png"
-	// 				className="h-full w-full lg:h-[615px] lg:w-[770px]"
-	// 			/>
-	// 		</div>
-	// 	),
-	// }
-
-	const sportData = data?.programs_data?.map((item) => {
+	const sportData = data?.programs_data?.map((item, i) => {
 		return {
-			icon: iconMap[item.name as keyof typeof iconMap] || null,
+			icon: maskingComponents[i].icon,
 			name: item.name,
 			image_url: item.image_url,
 			alt: item.alt_text,
-			component: getMaskingComponent(item.name as string, item.image_url as string),
-			reverse: reverse[item.name as keyof typeof reverse] || false,
+			component: maskingComponents[i].component(item.image_url as string),
+			reverse: maskingComponents[i].reverse,
 		}
 	})
 
